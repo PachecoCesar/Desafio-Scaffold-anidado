1565a5b1e88d98f41f9ce40308deed6279682df8
PachecoCesar <c86pacheco@gmail.com>
2019-09-12 12:45:49 -0300

    added playlist model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+require 'faker'
+10.times do
+  User.create(
+    name:Faker::Name.name,
+    email:Faker::Internet.email
+  )
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
